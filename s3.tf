aaeec004c7d22242369a81c2c9852aebf65ba580
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-20 19:35:40 +0900

    fix action

--- s3.tf
+++ s3.tf
@@ -14,6 +14,7 @@ resource "aws_iam_role_policy" "s3_service_account" {
   policy = "${data.aws_iam_policy_document.s3.json}"
   role   = "${aws_iam_role.s3_service_account.name}"
 }
+
 resource "aws_iam_role" "s3_service_account" {
   assume_role_policy = "${data.aws_iam_policy_document.s3_service_account.json}"
 }
@@ -32,7 +33,7 @@ data "aws_iam_policy_document" "s3_service_account" {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}:sub"
       values = [
-        "system:serviceaccount:default:vault-unseal"
+        "system:serviceaccount:default:s3-sa"
       ]
     }
   }
@@ -42,15 +43,26 @@ resource "aws_iam_policy" "s3" {
   name   = "s3_read_only"
   policy = "${data.aws_iam_policy_document.s3.json}"
 }
+
 data "aws_iam_policy_document" "s3" {
   statement {
     effect    = "Allow"
     resources = ["*"]
 
     actions = [
-      "s3:Get*",
-      "s3:List*",
+      "s3:*",
     ]
   }
 }
 
+
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terraform-bucket-test-hello-hello"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
